559be8205896c6312b3fd3badc959a2ffeb28d0d
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-11-12 10:00:28 +0700

    New Shopee

---
+++ Shopee/js/main.js
@@ -0,0 +1,12 @@
+// Config main slide carousel
+$('.owl-carousel.main-slide__carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    slideBy: 1
+})
