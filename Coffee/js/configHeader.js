0e868e523e10b4dcb1ea6f8b97028ce1423a2be1
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-12-02 22:27:43 +0700

    res

--- Coffee/js/configHeader.js
+++ Coffee/js/configHeader.js
@@ -1,16 +1,19 @@
-let elmBody = document.getElementById('main-wrapper');
-let elmHeader = document.getElementById('header');
+let elmToggleMenu = document.getElementById('toggle-menu');
+elmToggleMenu.addEventListener('click', function() {
+    if(elmToggleMenu.classList.contains('active')){
+        elmToggleMenu.classList.remove('active');
+        $('#toggle-menu .icon-menu').css('display', 'inline');
+        $('#toggle-menu .icon-cross').css('display', 'none');
+        $('#responsive-menu').css('transform', 'translateX(-100%)');
+    }else{
+        elmToggleMenu.classList.add('active');
+        $('#toggle-menu .icon-menu').css('display', 'none');
+        $('#toggle-menu .icon-cross').css('display', 'inline');
+        $('#responsive-menu').css('transform', 'translateX(0%)');
 
-let oldPos = 0
-elmBody.addEventListener('scroll', function() {
-    // let posTop = elmBody.scrollTop;
-    // if(posTop !== 0){
-    //     elmHeader.classList.add('fixed');
-    // }else{
-    //     elmHeader.classList.remove('fixed');
-    // }
-
-    console.log('scroll')
-
+    }
+    
 })
+
+
 
