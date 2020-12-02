c3e6bcf48cde83e8b198343f38870b7c738ef841
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-12-02 16:45:56 +0700

    responsive index.html

---
+++ Coffee/js/configHeader.js
@@ -0,0 +1,16 @@
+let elmBody = document.getElementById('main-wrapper');
+let elmHeader = document.getElementById('header');
+
+let oldPos = 0
+elmBody.addEventListener('scroll', function() {
+    // let posTop = elmBody.scrollTop;
+    // if(posTop !== 0){
+    //     elmHeader.classList.add('fixed');
+    // }else{
+    //     elmHeader.classList.remove('fixed');
+    // }
+
+    console.log('scroll')
+
+})
+
