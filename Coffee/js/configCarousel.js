efb833fd9e94e459407de0ef0294646a6983cb63
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-12-05 22:41:09 +0700

    coffee website

--- Coffee/js/configCarousel.js
+++ Coffee/js/configCarousel.js
@@ -4,9 +4,9 @@ $('.owl-carousel.main-slider__carousel').owlCarousel({
     nav: false,
     dots: true,
     items: 1,
-    // animateOut: 'fadeOut',
-    // autoplay: true,
-    // autoplayTimeout: 3000,
-    // autoplayHoverPause: true,
-    // slideBy: 1
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    slideBy: 1
 })
